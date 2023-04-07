50e8a7fe0c255b51cbd5f314e569be27699c6465
chenzhangyue <chenzhangyue01@weidian.com>
2023-04-07 23:05:54 +0800

    :rocket:  api doc

--- javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.lunasaw.webdav"},{"l":"io.github.lunasaw.webdav.config"},{"l":"io.github.lunasaw.webdav.properties"}];updateSearchResults();
